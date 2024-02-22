342b113704194ba7027385e1290ea3c06fb9f5f2
Cris Crawford <crawford.cris@gmail.com>
2024-02-22 01:50:56 +0000

    Finally got the right number.

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -4,13 +4,23 @@ with
     tripdata as (
         select * from {{ ref("stg_fhv_tripdata") }}
     ),
-    dim_zones as (select * from {{ ref("dim_zones") }} where borough != 'Unknown')
+    fhv_zones as (select * from {{ ref("fhv_zones") }} where borough != 'Unknown')
 select
-    *
+    tripdata.dispatching_base_num,
+    tripdata.pickup_datetime,
+    tripdata.dropoff_datetime,
+    tripdata.pickup_locationid,
+    pickup_zone.borough as pickup_borough,
+    pickup_zone.zone as pickup_zone,
+    tripdata.dropoff_locationid,
+    dropoff_zone.borough as dropoff_borough,
+    dropoff_zone.zone as dropoff_zone,
+    tripdata.sr_flag,
+    tripdata.affiliated_base_number
 from tripdata
 inner join
-    dim_zones as pickup_zone on tripdata.pickup_locationid = pickup_zone.locationid
+    fhv_zones as pickup_zone on tripdata.pickup_locationid = pickup_zone.locationid
 inner join
-    dim_zones as dropoff_zone
+    fhv_zones as dropoff_zone
     on tripdata.dropoff_locationid = dropoff_zone.locationid
 
--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -2,13 +2,11 @@
 
 with
     tripdata as (
-        select *, row_number() over (partition by dispatching_base_num, pickup_datetime) as rn
+        select *
         from {{ source("staging", "fhv_tripdata") }}
-        where dispatching_base_num is not null
     )
 select
     -- identifiers
-    {{ dbt_utils.surrogate_key(["dispatching_base_num", "pickup_datetime"]) }} as tripid,
     {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }} as dispatching_base_num,
     {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
     {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
@@ -19,8 +17,6 @@ select
     cast(pickup_datetime as timestamp) as pickup_datetime,
     cast(dropoff_datetime as timestamp) as dropoff_datetime,
 from tripdata
-where pickup_datetime > "2018-12-31 23:59:59" and pickup_datetime < "2020-01-01 00:00:00"
-and rn = 1
 
 -- dbt build --select <model.sql> --vars '{'is_test_run: false}'
-{% if var("is_test_run", default=true) %} limit 100 {% endif %}
+{% if var("is_test_run", default=false) %} limit 100 {% endif %}
